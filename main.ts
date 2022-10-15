9d78692122d32e205e794a0fc3b3ca2c7259f5f8
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2022-10-15 11:24:45 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -126,7 +126,7 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     otherSprite.destroy(effects.disintegrate, 500)
 })
 let HP: Sprite = null
-let Powerup: Sprite = null
+let Powerup2: Sprite = null
 let projectile2: Sprite = null
 let projectile5: Sprite = null
 let projectile4: Sprite = null
@@ -172,7 +172,7 @@ statusbar.positionDirection(CollisionDirection.Bottom)
 statusbar.setColor(7, 2)
 statusbar.value = 50
 statusbar.setPosition(80, 0)
-let Prox = sprites.create(img`
+let Prox2 = sprites.create(img`
     ................................................................................................................................................................
     ................................................................................................................................................................
     ................................................................................................................................................................
@@ -294,21 +294,191 @@ let Prox = sprites.create(img`
     ................................................................................................................................................................
     ................................................................................................................................................................
     `, SpriteKind.Prox)
-Prox.setFlag(SpriteFlag.Invisible, true)
-Prox.follow(Ship)
+Prox2.setFlag(SpriteFlag.Invisible, true)
+Prox2.follow(Ship)
 let Big_asteroids = [
-assets.image`A`,
-assets.image`B`,
-assets.image`C`,
-assets.image`D`
+img`
+    . . . . . . . . . c c 8 . . . . 
+    . . . . . . 8 c c c f 8 c c . . 
+    . . . c c 8 8 f c a f f f c c . 
+    . . c c c f f f c a a f f c c c 
+    8 c c c f f f f c c a a c 8 c c 
+    c c c b f f f 8 a c c a a a c c 
+    c a a b b 8 a b c c c c c c c c 
+    a f c a a b b a c c c c c f f c 
+    a 8 f c a a c c a c a c f f f c 
+    c a 8 a a c c c c a a f f f 8 a 
+    . a c a a c f f a a b 8 f f c a 
+    . . c c b a f f f a b b c c 6 c 
+    . . . c b b a f f 6 6 a b 6 c . 
+    . . . c c b b b 6 6 a c c c c . 
+    . . . . c c a b b c c c . . . . 
+    . . . . . c c c c c c . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . c c c c . . 
+    . c c c c c . c c c c c f c c . 
+    c c a c c c c c 8 f f c f f c c 
+    c a f a a c c a f f c a a f f c 
+    c a 8 f a a c a c c c a a a a c 
+    c b c f a a a a a c c c c c c c 
+    c b b a a c f 8 a c c c 8 c c c 
+    . c b b a b c f a a a 8 8 c c . 
+    . . . . a a b b b a a 8 a c . . 
+    . . . . c b c a a c c b . . . . 
+    . . . . b b c c a b b a . . . . 
+    . . . . b b a b a 6 a . . . . . 
+    . . . . c b b b 6 6 c . . . . . 
+    . . . . . c a 6 6 b c . . . . . 
+    . . . . . . . c c c . . . . . . 
+    `,
+img`
+    . . . . . . . . c c c c . . . . 
+    . . . . c c c c c c c c c . . . 
+    . . . c f c c a a a a c a c . . 
+    . . c c f f f f a a a c a a c . 
+    . . c c a f f c a a f f f a a c 
+    . . c c a a a a b c f f f a a c 
+    . c c c c a c c b a f c a a c c 
+    c a f f c c c a b b 6 b b b c c 
+    c a f f f f c c c 6 b b b a a c 
+    c a a c f f c a 6 6 b b b a a c 
+    c c b a a a a b 6 b b a b b a . 
+    . c c b b b b b b b a c c b a . 
+    . . c c c b c c c b a a b c . . 
+    . . . . c b a c c b b b c . . . 
+    . . . . c b b a a 6 b c . . . . 
+    . . . . . . b 6 6 c c . . . . . 
+    `,
+img`
+    . . . . . . c c c . . . . . . . 
+    . . . . . a a a c c c . . . . . 
+    . . . c a c f a a a a c . . . . 
+    . . c a c f f f a f f a c . . . 
+    . c c a c c f a a c f f a c . . 
+    . a b a a c 6 a a c c f a c c c 
+    . a b b b 6 a b b a a c a f f c 
+    . . a b b a f f b b a a c f f c 
+    c . a a a c c f c b a a c f a c 
+    c c a a a c c a a a b b a c a c 
+    a c a b b a a 6 a b b 6 b b c . 
+    b a c b b b 6 b c . c c a c . . 
+    b a c c a b b a c . . . . . . . 
+    b b a c a b a a . . . . . . . . 
+    a b 6 b b a c . . . . . . . . . 
+    . a a b c . . . . . . . . . . . 
+    `
 ]
 Small_asteroids = [
-assets.image`E`,
-assets.image`F`,
-assets.image`G`,
-assets.image`H`,
-assets.image`I`,
-assets.image`J`
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c c . . . . . . 
+    . . . . . . a b a a . . . . . . 
+    . . . . . c b a f c a c . . . . 
+    . . . . c b b b f f a c c . . . 
+    . . . . b b f a b b a a c . . . 
+    . . . . c b f f b a f c a . . . 
+    . . . . . c a a c b b a . . . . 
+    . . . . . . c c c c . . . . . . 
+    . . . . . . . c . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c . . . . . . 
+    . . . . . c a a a a . . . . . . 
+    . . . . . a a f f b a . . . . . 
+    . . . . c a b f f c b . . . . . 
+    . . . . c b b b a f c b . . . . 
+    . . . . c b a c a b b b . . . . 
+    . . . . . b b f f a a c . . . . 
+    . . . . . . a a b b c . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . c a a a a . . . . . . . 
+    . . . . a a f f b a . . . . . . 
+    . . . c a b f f c b . . . . . . 
+    . . . c b b b a f c b . . . . . 
+    . . . c b a c a b b b . . . . . 
+    . . . . b b f f a a c . . . . . 
+    . . . . . a a b b c . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c . . . . . . . . 
+    . . . . c a a a c . . . . . . . 
+    . . . c c f a b b c . . . . . . 
+    . . . b f f b f a a . . . . . . 
+    . . . b b a b f f a . . . . . . 
+    . . . c b f b b a c . . . . . . 
+    . . . . b a f c c . . . . . . . 
+    . . . . . b b c . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . c b a c . . . . . . 
+    . . . . c c b c f a c . . . . . 
+    . . . . a f b b b a c . . . . . 
+    . . . . a f f b a f c c . . . . 
+    . . . . c b b a f f c . . . . . 
+    . . . . . b b a f a . . . . . . 
+    . . . . . . c b b . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . c c . . . . . . . . 
+    . . . . c a f b c . . . . . . . 
+    . . . . b f f b c c . . . . . . 
+    . . . a a f b a b a c . . . . . 
+    . . . c a c b b f f b . . . . . 
+    . . . . b f f b f a b . . . . . 
+    . . . . a f f b b b a . . . . . 
+    . . . . . a b b c c . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `
 ]
 THRUSTER_VELOCITY = 5
 TORPEDO_SPEED = 100
@@ -337,7 +507,7 @@ forever(function () {
 })
 game.onUpdateInterval(10000, function () {
     if (Math.percentChance(50)) {
-        Powerup = sprites.createProjectileFromSide(img`
+        Powerup2 = sprites.createProjectileFromSide(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -355,7 +525,7 @@ game.onUpdateInterval(10000, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, randint(-10, 10), randint(-10, 10))
-        Powerup.setKind(SpriteKind.Food)
+        Powerup2.setKind(SpriteKind.Food)
     } else {
         HP = sprites.createProjectileFromSide(img`
             ....................
